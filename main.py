49d607bbb79fe00e4ce3bdf34411398da8d558bd
jvdevs <joaodarklegal@gmail.com>
2025-01-31 13:17:51 -0300

    Função para carregar e salvar tarefas do arquivo JSON

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ from tkinter import messagebox
 import json
 
 ARQUIVO_TAREFAS = "tarefas.json"
+
+
+def carregar_tarefas():
+    try:
+        with open(ARQUIVO_TAREFAS, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+
+def salvar_tarefas():
+    with open(ARQUIVO_TAREFAS, "w") as f:
+        json.dump(tarefas, f, indent=4)
